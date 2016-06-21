45420c18116be5a5a1fdf3f8123529c4dbd58312
Karl McGuinness <kmcguinness@okta.com>
2016-06-21 12:01:38 -0700

    init commit

---
+++ Okta.Owin.OpenIdConnect.Sample/App_Start/Startup.Auth.cs
@@ -0,0 +1,77 @@
+﻿//----------------------------------------------------------------------------------------------
+//    Copyright 2014 Microsoft Corporation
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//----------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+// The following using statements were added for this sample.
+using Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System.Configuration;
+using System.Globalization;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web.Helpers;
+using System.IdentityModel.Tokens;
+
+namespace Okta.Owin.OpenIdConnect.Sample
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            AntiForgeryConfig.UniqueClaimTypeIdentifier = "sub";
+
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+            app.UseOpenIdConnectAuthentication(
+                new OpenIdConnectAuthenticationOptions
+                {
+                    ClientId = ConfigurationManager.AppSettings["okta:ClientId"],
+                    ClientSecret = ConfigurationManager.AppSettings["okta:ClientSecret"],
+                    Authority = ConfigurationManager.AppSettings["okta:Issuer"],
+                    MetadataAddress = new Uri(new Uri(ConfigurationManager.AppSettings["okta:Issuer"]), "/.well-known/openid-configuration").ToString(),
+                    Scope = ConfigurationManager.AppSettings["okta:Scope"],
+                    RefreshOnIssuerKeyNotFound = true,
+                    TokenValidationParameters = new TokenValidationParameters
+                    {
+                        NameClaimType = "preferred_username",
+                        RoleClaimType = "groups"
+                    },
+                    Notifications = new OpenIdConnectAuthenticationNotifications
+                    {
+                        RedirectToIdentityProvider = (context) =>
+                        {
+                            context.ProtocolMessage.RedirectUri = context.OwinContext.Request.Uri.GetLeftPart(System.UriPartial.Authority);
+                            return Task.FromResult(0);
+                        },
+                        AuthenticationFailed = (context) =>
+                        {
+                            context.HandleResponse();
+                            context.Response.Redirect("/Error?message=" + context.Exception.Message);
+                            return Task.FromResult(0);
+                        }
+                    }
+                });
+        }
+    }
+}
---
+++ Okta.Owin.OpenIdConnect.Sample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Okta.Owin.OpenIdConnect.Sample.MvcApplication" Language="C#" %>
